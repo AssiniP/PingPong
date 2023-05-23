cfcc4dd8df77a399198d637a74c825a8921613a8
AssiniP <paulaassini04@gmail.com>
2023-05-23 17:13:20 -0300

    Se agrega clase de configuration

--- Configuration.php
+++ Configuration.php
@@ -1,1 +1,39 @@
 <?php
+include_once('helpers/MySqlDatabase.php');
+//include_once("helpers/MustacheRender.php");
+include_once('helpers/Router.php');
+
+//INCLUIR ACA MODELOS Y CONTROLADORES
+//Comento los mustache porque quiza los usemos ahora lmao
+//include_once('third-party/mustache/src/Mustache/Autoloader.php');
+
+class Configuration {
+    private $configFile = 'config/config.ini';
+
+    public function __construct() {
+    }
+
+    private function getArrayConfig() {
+        return parse_ini_file($this->configFile);
+    }
+
+    private function getRenderer() {
+        return new MustacheRender('view/partial');
+    }
+
+    public function getDatabase() {
+        $config = $this->getArrayConfig();
+        return new MariaDBDatabase(
+            $config['servername'],
+            $config['username'],
+            $config['password'],
+            $config['database']);
+    }
+
+    public function getRouter() {
+        return new Router(
+            $this,
+            "getController",
+            "list");
+    }
+}
