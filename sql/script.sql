f5e5c6748030ffe4a091c841cd126f5c4789f188
julicastro <castroejulian@gmail.com>
2023-05-27 20:51:36 -0300

    script sql. tablas

--- sql/script.sql
+++ sql/script.sql
@@ -2,37 +2,83 @@ CREATE DATABASE pingPong;
 
 USE pingPong;
 
-create table rol (
-id int  auto_increment primary key,
-rol  varchar(150) not null
+CREATE TABLE Rol (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    rol VARCHAR(150) NOT NULL
 );
 
-insert into rol (rol) values ('Administrador');
-insert into rol (rol) values ('Editor');
-insert into rol (rol) values ('Jugador');
-
-create table genero (
-id int  auto_increment primary key,
-nombre  varchar(150) not null
+CREATE TABLE Genero (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(150) NOT NULL
 );
 
-insert into genero (nombre) values ('Masculino');
-insert into genero (nombre) values ('Femenino');
-insert into genero (nombre) values ('Prefiero no cargarlo');
+CREATE TABLE Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nickName VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    nombre VARCHAR(150) NOT NULL,
+    descripcion VARCHAR(150) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    ubicacion VARCHAR(150) NOT NULL,
+    imagenPerfil VARCHAR(150) NOT NULL,
+    pais VARCHAR(150) NOT NULL,
+    ciudad VARCHAR(150) NOT NULL,
+    idRol INT NOT NULL,
+    fecharegistro DATE,
+    idGenero INT NOT NULL,
+    FOREIGN KEY (idGenero) REFERENCES Genero(id),
+    FOREIGN KEY (idRol) REFERENCES Rol(id)
+);
 
-CREATE TABLE usuario (
-id int auto_increment primary key,
-nickName varchar(50) not null,
-password varchar(50) not null,
-nombre varchar(150) not null,
-email  varchar(150) not null,
-ubicacion  varchar(150) not null,
-imagenPerfil  varchar(150) not null,
-pais  varchar(150) not null,
-ciudad  varchar(150) not null,
-idRol int not null, 
-fecharegistro date,
-idGenero int not null,
-FOREIGN KEY (idGenero) REFERENCES genero(id),
-FOREIGN KEY (idRol) references rol(id)
+CREATE TABLE Partida (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fecha DATE NOT NULL,
+    tiempo TIME NOT NULL
+);
+
+CREATE TABLE Jugada (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idPartida INT NOT NULL,
+    idUsuario INT NOT NULL,
+    FOREIGN KEY (idPartida) REFERENCES Partida(id),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
+);
+
+CREATE TABLE Opcion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    opcion1 VARCHAR(255) NOT NULL,
+    opcion2 VARCHAR(255) NOT NULL,
+    opcion3 VARCHAR(255) NOT NULL,
+    opcion4 VARCHAR(255) NOT NULL,
+    respuestaCorrecta INT NOT NULL
+);
+
+CREATE TABLE Categoria (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Pregunta (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    pregunta VARCHAR(255) NOT NULL,
+    idOpcion INT NOT NULL,
+    idCategoria INT NOT NULL,
+    idUsuario INT NOT NULL,
+    cantidadAciertos INT NOT NULL,
+    cantidadOcurrencias INT NOT NULL,
+    idJugada INT NOT NULL,
+    FOREIGN KEY (idOpcion) REFERENCES Opcion(id),
+    FOREIGN KEY (idCategoria) REFERENCES Categoria(id),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(id),
+    FOREIGN KEY (idJugada) REFERENCES Jugada(id)
+);
+
+CREATE TABLE PreguntaReportada (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    idPregunta INT NOT NULL,
+    idUsuario INT NOT NULL,
+    fecha DATE NOT NULL,
+    motivo VARCHAR(255) NOT NULL,
+    FOREIGN KEY (idPregunta) REFERENCES Pregunta(id),
+    FOREIGN KEY (idUsuario) REFERENCES Usuario(id)
 );
