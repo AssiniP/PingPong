1b2e29f45fbff435ae686cea784161bb434e8a50
julicastro <castroejulian@gmail.com>
2023-05-31 02:17:23 -0300

    Cambios en las tablas Partida y Jugada, entre ellos la FK de Usuario en jugada pasa a partida. El (Nara pará no me grites). Métodos en el modelo y controlador para agregar partida con id de usuario x defecto hasta q se muevan las sesiones al modelo. X ahora no me hace el insert en partida…

--- controller/PartidaController.php
+++ controller/PartidaController.php
@@ -19,8 +19,14 @@ class PartidaController{
     }
 
     public function jugar(){
-
-
+        $preguntas = $this->partidaModel->getPreguntasConOpciones();
+        $data = array('preguntas' => $preguntas);
+        $this->renderer->render('partida', $data);
     }
 
+    public function crearPartida(){
+        $idUsuario = 1; /* x defecto 1 xq se q van a mover la sesión de los controller. */
+        $this->partidaModel->insertPartida($idUsuario);
+        header('location: /partida/list');
+    }
 }
--- model/PartidaModel.php
+++ model/PartidaModel.php
@@ -8,5 +8,24 @@ class PartidaModel
         $this->database = $database;
     }
 
+    public function getPreguntas() {
+        return $this->database->query("SELECT * FROM Pregunta");
+    }
+
+    public function getPreguntasConOpciones() {
+        $query = "SELECT p.id, p.pregunta, o.opcion1, o.opcion2, o.opcion3, o.opcion4, o.respuestaCorrecta
+                  FROM Pregunta p
+                  INNER JOIN Opcion o ON p.idOpcion = o.id";
+        return $this->database->query($query);
+    }
+
+    public function insertPartida($idUsuario) {
+        $fechaActual = date("d-m-Y");
+        $tiempo = 0;
+        $query = "INSERT INTO Partida (fecha, tiempo, idUsuario) VALUES ('$fechaActual', $tiempo, $idUsuario)";
+        return $this->database->query($query);
+    }
+
+
 
 }
