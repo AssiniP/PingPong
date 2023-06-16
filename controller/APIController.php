c650f04995cfbd2db8d00a65018209f6bc9e24ea
NaraHumanoUnlam <nhumano207@alumno.unlam.edu.ar>
2023-06-15 23:55:51 -0300

    se sube abm en api de preguntas y algunas cositas de la vista

--- controller/APIController.php
+++ controller/APIController.php
@@ -9,8 +9,53 @@ class APIController
     public function list() {
         header('Content-Type: application/json');
         $api = new ApiPreguntas();
-        $idUsuario = $_GET['usuario'];
-        $api->getAll($idUsuario);
 
+            if(isset($_GET['usuario'])){
+                $idUsuario = $_GET['usuario'];
+                $api->getAll($idUsuario);
+            }
+
+
+
+    }
+
+    public function altaPregunta()
+    {
+        header('Content-Type: application/json');
+        $api = new ApiPreguntas();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $jsonData = file_get_contents('php://input');
+            if($jsonData!= null){
+                $body = json_decode($jsonData);
+                echo $api->altaPregunta($body->pregunta->valor,$body->pregunta->opcion1, $body->pregunta->opcion2,$body->pregunta->opcion3, $body->pregunta->opcion4, $body->pregunta->correcta,$body->pregunta->categoria,$body->usuario);
+            }
+
+
+        }
+    }
+
+    public  function bajaPregunta(){
+        header('Content-Type: application/json');
+        $api = new ApiPreguntas();
+        if(isset($_GET['pregunta'])){
+            $idPregunta = $_GET['pregunta'];
+            echo $api->bajaPregunta($idPregunta);
+        }
+    }
+
+    public function editarPregunta(){
+        header('Content-Type: application/json');
+        $api = new ApiPreguntas();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+            $jsonData = file_get_contents('php://input');
+            if($jsonData!= null){
+                $body = json_decode($jsonData);
+                echo $api->modificarPregunta($body->pregunta->id,$body->pregunta->valor,$body->pregunta->opcion1, $body->pregunta->opcion2,$body->pregunta->opcion3, $body->pregunta->opcion4, $body->pregunta->correcta,$body->pregunta->categoria);
+            }
+
+
+        }
     }
 }
--- sql/inserts.sql
+++ sql/inserts.sql
@@ -20,8 +20,9 @@ INSERT INTO Categoria (nombre,color) VALUES ('Deportes','green');
 INSERT INTO pingpong.usuario (nickName, password, nombre, email, imagenPerfil, pais, ciudad, idRol, cuentaValida,idGenero, fechaNacimiento, latitud, longitud)
 VALUES ( 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin', 'admin@mail.com', 'admin.jpg', 'Argentina', 'San Justo', '1', '1', '3','1974-05-25',-58.5614947,-34.6686986);
 INSERT INTO pingpong.usuario (nickName, password, nombre, email, imagenPerfil, pais, ciudad, idRol, cuentaValida,idGenero, fechaNacimiento, latitud, longitud)
-VALUES ( 'user', '21232f297a57a5a743894a0e4a801fc3', 'usuario de prueba', 'user@gmail.com', 'user.jpg', 'Argentina', 'San Justo', '1', '1', '2','1989-05-25',-58.5614947,-34.6686986);
-
+VALUES ( 'user', '202cb962ac59075b964b07152d234b70', 'usuario de prueba', 'user@gmail.com', 'user.jpg', 'Argentina', 'San Justo', '3', '1', '2','1989-05-25',-58.5614947,-34.6686986);
+INSERT INTO pingpong.usuario (nickName, password, nombre, email, imagenPerfil, pais, ciudad, idRol, cuentaValida,idGenero, fechaNacimiento, latitud, longitud)
+VALUES ( 'editor', '202cb962ac59075b964b07152d234b70', 'editor', 'editor@gmail.com', 'editor.jpg', 'Argentina', 'San Justo', '2', '1', '2','1989-05-25',-58.5614947,-34.6686986);
 -- Pregunta 1
 INSERT INTO Pregunta (pregunta, idCategoria, idUsuario, cantidadAciertos, cantidadOcurrencias,opcion1,opcion2, opcion3, opcion4, respuestaCorrecta)
 VALUES ('¿Cuál es la capital de Francia?', 1, 1, 0, 0,'París', 'Londres', 'Roma', 'Madrid', 1);
