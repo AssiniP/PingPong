d57e137e5867fc2d1defeaab12a007d1e3a74522
AssiniP <paulaassini04@gmail.com>
2023-06-25 01:53:47 -0300

    Se empiezan a filtrar las querys posibles

--- controller/AdminController.php
+++ controller/AdminController.php
@@ -13,6 +13,7 @@ class AdminController
     public function list()
     {
         $_SESSION['jugando'] = true;
+
         $arrayDatos = $this->adminModel->adminModelMethodsTest();
         $data['totalUsuarios'] = $arrayDatos['arrayDatos']['totalUsuarios'];
         $data['totalJugadores'] = $arrayDatos['arrayDatos']['totalJugadores'];
@@ -26,9 +27,9 @@ class AdminController
         $data['porcentajePreguntasAcertadasPorUsuario'] = $arrayDatos['arrayDatos']['porcentajePreguntasAcertadasPorUsuario'];
         $data['cantidadUsuariosPorSexo'] = $arrayDatos['arrayDatos']['cantidadUsuariosPorSexo'];
         $data['partidasNuevasDesdeFecha'] = $arrayDatos['arrayDatos']['partidasNuevasDesdeFecha'];
-        $data['nombreMes'] = $arrayDatos['arrayDatos']['nombreMes'];
         $data['imagePath'] = $arrayDatos['arrayDatos']['imagePath'];
-        $data['mesesList'] = $arrayDatos['arrayDatos']['mesesList'];
+
+
 
         $data['balanceTrampitas'] = $arrayDatos['arrayDatos']['balanceTrampitas'];
         $data['cantidadTrampitas'] = $arrayDatos['arrayDatos']['cantidadTrampitas'];
